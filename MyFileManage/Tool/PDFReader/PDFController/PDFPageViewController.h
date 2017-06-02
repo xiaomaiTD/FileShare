a0e9c5e5693bad708727f73bae5b5efbf253ea47
Viterbi1992 <wangchaonihao@163.com>
2017-06-02 09:43:25 +0800

    更改阅读PDF相关文件的目录，更改PDF架构

---
+++ MyFileManage/Tool/PDFReader/PDFController/PDFPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDFPageViewController.h
+//  MyFileManage
+//
+//  Created by 掌上先机 on 2017/6/2.
+//  Copyright © 2017年 wangchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDFPageViewController : UIViewController
+
+@end
---
+++ MyFileManage/Tool/PDFReader/PDFController/PDFPageViewController.m
@@ -0,0 +1,30 @@
+//
+//  PDFPageViewController.m
+//  MyFileManage
+//
+//  Created by 掌上先机 on 2017/6/2.
+//  Copyright © 2017年 wangchao. All rights reserved.
+//
+
+#import "PDFPageViewController.h"
+
+@interface PDFPageViewController ()
+
+@end
+
+@implementation PDFPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+    self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    
+    
+   
+}
+
+
+
+@end
