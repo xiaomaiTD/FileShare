eda1b87b689c8bbd6940e18f64285c1480791de3
ViterbiDevelopment <wangchaonihao@163.com>
2018-03-08 00:28:26 +0800

    abandom scroll

--- MyFileManage/Home/Controller/openImageViewController.m
+++ MyFileManage/Home/Controller/openImageViewController.m
@@ -36,6 +36,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.automaticallyAdjustsScrollViewInsets = NO;
     self.navigationController.navigationBar.y = -88;
     _navISHidden = YES;
     self.title = _model.fileName;
