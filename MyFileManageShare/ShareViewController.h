eb12e4eaddbbbaf26dcfee1200b00e15d4a6e5f4
ViterbiDevelopment <wangchaonihao@163.com>
2018-03-19 22:55:18 +0800

    add app extension

---
+++ MyFileManageShare/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  MyFileManageShare
+//
+//  Created by Viterbi on 2018/3/19.
+//  Copyright © 2018年 wangchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
