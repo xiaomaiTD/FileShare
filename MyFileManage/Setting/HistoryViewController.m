278ded7f9ee453c25de0166cb17f0fbea52a5899
ViterbiDevelopment <wangchaonihao@163.com>
2018-03-30 15:25:14 +0800

    finish history

--- MyFileManage/Setting/HistoryViewController.m
+++ MyFileManage/Setting/HistoryViewController.m
@@ -5,9 +5,11 @@
 //  Created by Viterbi on 2018/3/27.
 //  Copyright © 2018年 wangchao. All rights reserved.
 //
-
+#import "UIViewController+Extension.h"
 #import "HistoryViewController.h"
 #import "SettingHistoryCell.h"
+#import "FolderFileManager.h"
+#import "EasyAlertView.h"
 #import "FMDBTool.h"
 #import "fileModel.h"
 
@@ -39,7 +41,38 @@
         self.dataArray = data.mutableCopy;
         [self.tableView reloadData];
     }];
-    
+    @weakify(self);
+    UIButton *deleteBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [deleteBtn setImage:[UIImage imageNamed:@"delete"] forState:UIControlStateNormal];
+    [deleteBtn addTargetWithBlock:^(UIButton *sender) {
+        @strongify(self);
+        if (self.dataArray.count == 0) {
+            return;
+        }
+        NSArray *actionArray = @[@{@"确定":@(0)},@{@"取消":@(0)}];
+        EasyAlertView *alert = [[EasyAlertView alloc] initWithType:AlertViewAlert andTitle:@"是否清空数据" andActionArray:actionArray andActionBloc:^(NSString *title,NSInteger index) {
+            if (index == 0) {
+                [self clearData];
+            }
+        }];
+        [alert showInViewController:self];
+    }];
+    [self addRigthItemWithCustomView:deleteBtn];
+}
+
+-(void)clearData{
+    [self showMessageWithTitle:@"正在清除.."];
+    [[FMDBTool shareInstance] deleteAllHistoryModel:^(BOOL success) {
+        [self hidenMessage];
+        if (success) {
+            [self showSuccess];
+            [self.dataArray removeAllObjects];
+            NSIndexSet *set = [[NSIndexSet alloc] initWithIndex:0];
+            [self.tableView reloadSections:set withRowAnimation:UITableViewRowAnimationAutomatic];
+        }else{
+            [self showError];
+        }
+    }];
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -54,7 +87,11 @@
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     
     fileModel *model = self.dataArray[indexPath.row];
-    [self openVCWithModel:model];
+    if ([[FolderFileManager shareInstance] judgePathIsExits:model.fullPath]) {
+        [self openVCWithModel:model];
+    }else{
+        [self showErrorWithTitle:@"此文件已经被移动了"];
+    }
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
