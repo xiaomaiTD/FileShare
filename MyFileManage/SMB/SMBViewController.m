c03375a5d915a31df72e2d8e7551d2b6c46d81b4
ViterbiDevelopment <wangchaonihao@163.com>
2018-04-10 23:28:28 +0800

    add list

--- MyFileManage/SMB/SMBViewController.m
+++ MyFileManage/SMB/SMBViewController.m
@@ -9,7 +9,10 @@
 #import "SMBViewController.h"
 #import <SMBClient/SMBClient.h>
 
-@interface SMBViewController ()
+@interface SMBViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property(nonatomic,strong)UITableView *myTable;
+@property(nonatomic,strong)NSMutableArray *dataSource;
 
 @end
 
@@ -17,7 +20,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.myTable = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    self.myTable.tableFooterView = [[UIView alloc] init];
+    self.myTable.delegate = self;
+    self.myTable.dataSource = self;
+    [self.view addSubview:self.myTable];
+    
+    [self discoveryDevice];
+}
+
+-(void)discoveryDevice{
   
+    [[SMBDiscovery sharedInstance] startDiscoveryOfType:SMBDeviceTypeAny added:^(SMBDevice *device) {
+        NSLog(@"Device added: %@", device);
+        [self.dataSource addObject:device];
+        [self.myTable reloadData];
+    } removed:^(SMBDevice *device) {
+        [self.dataSource removeObject:device];
+        [self.myTable reloadData];
+        NSLog(@"Device removed: %@", device);
+    }];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    if (self.dataSource.count > 0) {
+        SMBDevice *device = self.dataSource[indexPath.row];
+        cell.textLabel.text = device.host;
+    }
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+  return self.dataSource.count;
 }
 
 
